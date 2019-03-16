29675be7b43542b9a2b4502d077553c7fb2a8a59
taka5hi <n.takashi930@gmail.com>
2019-03-16 14:23:48 +0900

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,6 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator version 2</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
